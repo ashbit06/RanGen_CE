d2a69d54ff11e37c3458f67c9cd8bcb4a1d395e0
ashbit_ <ashton.hovey@gmail.com>
2024-12-12 10:45:38 -0500

    spike is resized to 16x16

--- src/gfx/spike.h
+++ src/gfx/spike.h
@@ -6,10 +6,10 @@ extern "C" {
 #endif
 
 #define spike_width 16
-#define spike_height 17
-#define spike_size 274
+#define spike_height 16
+#define spike_size 258
 #define spike ((gfx_sprite_t*)spike_data)
-extern unsigned char spike_data[274];
+extern unsigned char spike_data[258];
 
 #ifdef __cplusplus
 }
